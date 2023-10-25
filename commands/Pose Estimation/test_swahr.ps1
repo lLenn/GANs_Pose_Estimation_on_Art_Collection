e341341b2cd23ea1be4428a6b9cb04f1741c0dd7
Tristan <tverheecke@hotmail.com>
2023-10-25 20:21:05 +0200

    Pose estimation

--- commands/Pose Estimation/test_swahr.ps1
+++ commands/Pose Estimation/test_swahr.ps1
@@ -1,4 +1,4 @@
-Set-Location lib\SWAHR-HumanPose
+Set-Location lib\SWAHR
 
 python .\tools\dist_valid.py --world_size 2 --cfg ../../study/models/w32_512_adam_lr1e-3.yaml TEST.MODEL_FILE ./models/pose_coco/pose_higher_hrnet_w32_512.pth DATASET.ROOT ../../../data/coco_data/
 
