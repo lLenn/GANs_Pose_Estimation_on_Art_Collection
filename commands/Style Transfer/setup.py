8bef4ff7f854ba83605ab60d5960e72771f35d16
Tristan <tverheecke@hotmail.com>
2023-12-19 00:38:16 +0100

    bug

--- commands/Style Transfer/setup.py
+++ commands/Style Transfer/setup.py
@@ -7,10 +7,10 @@ if os.path.exists("CycleGAN"):
         packages=['CycleGAN', 'CycleGAN.data', 'CycleGAN.models', 'CycleGAN.options', 'CycleGAN.util']
     )
     
-if os.path.exists("StarGan"):
+if os.path.exists("StarGAN"):
     setup(
-        name='StarGan',
-        packages=['StarGan']
+        name='StarGAN',
+        packages=['StarGAN']
     )
 
 if os.path.exists("UGATITLib"):
