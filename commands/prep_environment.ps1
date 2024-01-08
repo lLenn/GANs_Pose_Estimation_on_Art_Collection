d8bb6bf6535bfa98fd6a577f62ceb0cc2fd92742
Tristan <tverheecke@hotmail.com>
2024-01-08 17:22:48 +0100

    Install mmcv

--- commands/prep_environment.ps1
+++ commands/prep_environment.ps1
@@ -4,6 +4,8 @@ python -m pip install --upgrade pip
 # Install PyTorch
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 
+./Pose\ Estimation/install_mmcv.sh
+
 # Create tmp folder for install
 if(-Not (Test-Path -Path lib)) {
     New-Item -Name lib -ItemType directory
