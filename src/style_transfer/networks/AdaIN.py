db26d2972bd87b6c228a4189099e567915d85294
Tristan <tverheecke@hotmail.com>
2024-01-21 01:00:57 +0100

    Head vgg prop

--- src/style_transfer/networks/AdaIN.py
+++ src/style_transfer/networks/AdaIN.py
@@ -7,6 +7,7 @@ class AdaIN:
     def __init__(self, config):
         self.config = config
         self.vgg = vgg
+        self.head_vgg = nn.Sequential(*list(vgg.children())[:31])
         self.decoder = decoder
         
     def loadModel(self, paths=None):
@@ -16,16 +17,16 @@ class AdaIN:
         else:
             self.vgg.load_state_dict(torch.load(paths["vgg"]))
             self.decoder.load_state_dict(torch.load(paths["decoder"]))
+        self.head_vgg = nn.Sequential(*list(vgg.children())[:31])
             
     def transformTo(self, content, style):
         alpha = self.config.alpha
         device = self.config.device
         
-        vgg = nn.Sequential(*list(self.vgg.children())[:31])
-        vgg.eval()
+        self.head_vgg.eval()
         self.decoder.eval()
         
-        vgg.to(device)
+        self.head_vgg.to(device)
         self.decoder.to(device)
         
         if self.config.preserve_color:
@@ -33,8 +34,8 @@ class AdaIN:
         style = style.to(device).unsqueeze(0)
         content = content.to(device).unsqueeze(0)
         with torch.no_grad():
-            content_f = vgg(content)
-            style_f = vgg(style)
+            content_f = self.head_vgg(content)
+            style_f = self.head_vgg(style)
             feat = adaptive_instance_normalization(content_f, style_f)
             feat = feat * alpha + content_f * (1 - alpha)
             return self.decoder(feat)
