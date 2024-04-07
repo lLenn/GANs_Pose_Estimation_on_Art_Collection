1fff3a813192e5e0dd980205c14325e9527a11be
Tristan <tverheecke@hotmail.com>
2024-04-07 11:44:03 +0200

    device

--- src/test_style_transfer.py
+++ src/test_style_transfer.py
@@ -56,7 +56,7 @@ def measure(rank, world_size, num_workers, batch_size, dataset_directory, real_d
         # "style_transfer/config/stargan.yaml"
         config = StarGANConfig.create(config_file, options=options)
         network = StarGAN(config)
-        network.to(device)
+        network.model.to(device)
         network.loadModel(config.checkpoint_dir, "latest")
     else:
         raise Exception("Model not recognized") 
